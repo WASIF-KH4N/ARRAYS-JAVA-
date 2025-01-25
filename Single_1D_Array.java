8ca18dcc086a7eccad56f3bb3ebfead0fe5c4500
Wasif Khan <m.wasif9905@gmail.com>
2025-01-25 21:56:24 +0500

    initial commit

---
+++ Single_1D_Array.java
@@ -0,0 +1,29 @@
+ class Single_1D_Array {
+    public static void main(String []args){
+        /*int arr[]=new int [5];
+        System.out.println(arr[0]=1);
+        System.out.println(arr[4]=7);*/
+
+       /* int arr[]={1,24,7,6,3,2};
+       // System.out.println(arr.length); //6
+       // System.out.println(arr[4]); //3
+       /* for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+       System.out.println();*/
+      /*  String array[]=new String[4];
+        System.out.println(array[0]="Wasif");
+        System.out.print(array.length);*/
+        /*int arr[] = new int [-4]; // NegativeArraySizeException
+       System.out.println(arr);
+
+
+        int a[] ={1,2,3};           // ArrayIndexOutOfBoundsException
+        System.out.print(a[3]);*/
+        String name[]={"Wasif","Wajahat","Saad","Hamza","Taimoor","Hassan"};
+        for(int i=0;i<name.length;i++){
+            System.out.print(name[i]+ " , ");
+        }
+
+    }
+}
